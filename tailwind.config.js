b62610dcf7761211fe3cb326bd28af635f527f36
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2024-04-04 09:55:34 -0500

    feat: :art: Basic HTML structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 module.exports = {
 	content: ['./**/*.html'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				amber: 'hsl(38, 63%, 58%)',
+				coral: 'hsl(20, 64%, 65%)',
+				'pale-yellow': 'hsl(42, 73%, 85%)',
+				'dark-slate': 'hsl(33, 52%, 28%)',
+				charcoal: 'hsl(0, 0%, 20%)',
+			},
+			screens: {
+				tablet: '768px',
+				desktop: '1024px',
+			},
+			spacing: {
+				128: '32rem',
+				144: '36rem',
+				screen_130: 'calc(100vh - 130px)',
+			},
+		},
 	},
 	plugins: [],
 }
